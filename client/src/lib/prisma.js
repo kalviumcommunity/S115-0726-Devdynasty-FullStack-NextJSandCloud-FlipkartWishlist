2066ffaee64781552585e2953c3ac8516ce88dcd
Atharv137 <hargudetejas17@gmail.com>
2026-08-12 11:51:37 +0530

    fix: resolve cart badge internal server error

--- client/src/lib/prisma.js
+++ client/src/lib/prisma.js
@@ -1,16 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import ws from 'ws';
-
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 const globalForPrisma = globalThis;
